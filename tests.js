e2e3e0c913dfd4155c98a97a27055d23c8fb12e8
Crisktina <crishidalgolopez@gmail.com>
2022-11-08 12:20:50 +0100

    test solved

--- tests.js
+++ tests.js
@@ -3,8 +3,12 @@ const expect = chai.expect;
 
 describe("Variables en JS", () => {
     it("Completa todas las constantes que faltan", () => {
-
         const a = 10;
+        const b = 1.34;
+        const c = true;
+        const d = "Buenos días";
+        const e = "Pepe";
+        const f = "Buenos días, pepe";
 
         expect(a).to.equal(10);
         expect(b).to.equal(1.34);
@@ -12,12 +16,18 @@ describe("Variables en JS", () => {
         expect(d).to.equal("Buenos días");
         expect(e).to.equal("Pepe");
         expect(f).to.equal("Buenos días, pepe");
-    })
+    });
 
     it("Completa todas las variables que faltan para que las operaciones resulten correctamente", () => {
-
         let a = 11;
-
+        let b = 0.34;
+        let c = 10;
+        let d = -340;
+        let e = 0;
+        let f = 10;
+        let g = 3;
+        let x = 2;
+        let y = 1;
         expect(a + b).to.equal(11.34);
         expect(a * c).to.equal(110);
         expect(d - e).to.equal(-340);
@@ -25,52 +35,52 @@ describe("Variables en JS", () => {
         expect(f ** g).to.equal(1000);
         expect(x % 2).to.equal(0);
         expect(y % 2).to.equal(1);
-    })
+    });
 
     it("Completa todas las variables que faltan para que se cumplan las condiciones", () => {
-
         let a = 10;
-
+        let b = 100;
+        let c = undefined;
+        let d = false;
+        let n = "B";
         expect(a > 9).to.be.true;
         expect(a < 11).to.be.true;
         expect(b === 100).to.be.true;
         expect(c === undefined).to.be.true;
         expect(d !== "Hello").to.be.true;
-        expect(n.startsWith('A')).to.be.false;
-    })
-})
+        expect(n.startsWith("A")).to.be.false;
+    });
+});
 
 describe("condicionales en javascript", () => {
     it("completa el valor del resultado esperado (expected)", () => {
-
         let name = "Pepe";
 
         let result = "KO";
 
         // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/length
         if (name.length === 6) {
-            result = "OK"
+            result = "OK";
         }
 
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
+        // substituye ??? por el valor que tiene la variable "result"
+        expect(result === "KO").to.be.true;
+    });
 
     it("comparando cadenas (strings)", () => {
-
-        let a = 'a';
-        let b = 'b';
+        let a = "a";
+        let b = "b";
         let result;
 
         if (a < b) {
-            result = 'pikachu';
+            result = "pikachu";
         } else {
-            result = 'charmander';
+            result = "charmander";
         }
 
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
+        // substituye ??? por el valor que tiene la variable "result"
+        expect(result === "pikachu").to.be.true;
+    });
 
     it("Interpolación de cadenas (strings) (1)", () => {
         let n = "Piña";
@@ -85,9 +95,9 @@ describe("condicionales en javascript", () => {
             result = "Pizza con " + m;
         }
 
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
+        // substituye ??? por el valor que tiene la variable "result"
+        expect(result === "Pizza con Pepperoni").to.be.true;
+    });
 
     it("Interpolación de cadenas (strings) (2)", () => {
         let n = "Piña";
@@ -102,13 +112,12 @@ describe("condicionales en javascript", () => {
             result = `Pizza con ${m}`;
         }
 
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
-})
+        // substituye ??? por el valor que tiene la variable "result"
+        expect(result === "Pizza con Piña").to.be.true;
+    });
+});
 
 describe("Funciones en JS", () => {
-
     function f(a, b, c) {
         return b - a + 4 * c;
     }
@@ -118,7 +127,6 @@ describe("Funciones en JS", () => {
     }
 
     function h(name) {
-        
         // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/length
         if (name.length <= 4) {
             return "too short";
@@ -132,31 +140,25 @@ describe("Funciones en JS", () => {
     }
 
     it("Cuál es el resultado de invocar la función? (1)", () => {
-
         let result = f(10, 30, 2);
 
-
-        // substituye "???" por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
+        // substituye "???" por el valor que tiene la variable "result"
+        expect(result === 28).to.be.true;
+    });
 
     it("Cuál es el resultado de invocar la función? (2)", () => {
-
         let result = g("Cerebro", "Pinky");
 
-
-        // substituye "???" por el valor que tiene la variable "result"        
-        expect(result === "???").to.be.true;
-    })
+        // substituye "???" por el valor que tiene la variable "result"
+        expect(result === "Son Pinky y Cerebro").to.be.true;
+    });
 
     it("Cuál es el resultado de invocar la función? (3)", () => {
-
         // substituye "???" por el resultado en cada caso
-        expect(h("khaleesi mother of dragons breaker of chains") === "???").to.be.true;
-        expect(h("sarah") === "???").to.be.true;
-        expect(h("bob") === "???").to.be.true;
-        expect(h("robertson") === "???").to.be.true;
-    })
-
-})
-
+        expect(h("khaleesi mother of dragons breaker of chains") === "very long").to
+            .be.true;
+        expect(h("sarah") === "adecquate").to.be.true;
+        expect(h("bob") === "too short").to.be.true;
+        expect(h("robertson") === "long").to.be.true;
+    });
+});
